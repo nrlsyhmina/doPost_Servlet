a16bf16847a677ff86707de100c94dbe4c665679
Nurul Syahmina <48874599+nrlsyhmina@users.noreply.github.com>
2020-11-11 18:27:37 +0800

    Add files via upload

---
+++ LabServlet/src/HelloWorld.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("<h2>");
+		pw.println("Username: " + username);
+		pw.println("</h2><h2>");
+		pw.println("Password: " + password);
+		pw.println("</h2>");
+	}
+
+}
